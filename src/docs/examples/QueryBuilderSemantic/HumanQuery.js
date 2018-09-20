f05bc263fafa074bec976d806e80b29c36d5a6c6
Jacques Nel <jacques@playlogix.com>
2018-09-20 11:03:42 +0200

    chore: added human query format example

---
+++ src/docs/examples/QueryBuilderSemantic/HumanQuery.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import QueryBuilderSemantic from 'react-query-builder-semantic/lib/QueryBuilderSemantic';
+
+
+const fields = [
+    { value: 'firstName', text: 'First Name' },
+    { value: 'lastName', text: 'Last Name' },
+    { value: 'age', text: 'Age' },
+    { value: 'address', text: 'Address' },
+    { value: 'phone', text: 'Phone' },
+    { value: 'email', text: 'Email' },
+    { value: 'twitter', text: 'Twitter' },
+];
+
+/** QueryBuilderSemantic with human query string format */
+export default class HumanQuery extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            query:null
+        };
+        this.logQuery = this.logQuery.bind(this)
+    }
+
+    logQuery(query) {
+        this.setState({ query });
+    }
+
+    render() {
+        return (
+            <div className="flex-box">
+                <div className="scroll">
+                    <QueryBuilderSemantic fields={fields}
+                                          query={this.state.query}
+                                          onQueryChange={this.logQuery} />
+                </div>
+                <div className="shrink query-log scroll">
+                    <h4>Human Query String Format</h4>
+                    <pre>{QueryBuilderSemantic.getQueryString(this.state.query)}</pre>
+                </div>
+            </div>
+        );
+    }
+}
